e89cd068e97bce0c134d4d76f1f2ee5ed6fc1d98
Jas <c.jasminher@gmail.com>
2019-06-01 21:48:40 -0400

    fix: Fixed the login when user inputs correct and incorrect password, and when fields are empty

--- password.js
+++ password.js
@@ -4,26 +4,24 @@
 // c.	Your page should also include an <h1> tag. If the information in the form is correct, have JavaScript change the text in the <h1>.HTML page. It should include inputs for a username, email, and password. Also a submit button.
 
 
-let key = '12345678';
-let userN = document.getElementById("username").value;
-let emailA = document.getElementById("email").value;
-let passW = document.getElementById("password").value;
 
+let key = '12345678';
 
 
 function password(e) {
-    if ((passW == key) && (userN !== "") && (emailA !== "")) {
-        document.getElementById("heading").innerHTML = "Correct Sign In";
-        e.preventDefault();
-        } else if ((passW !== "") && (userN == "") && (emailA == "") && (passW !== key)) {
-            alert("fill in fields and wrong password")
-            e.preventDefault();
-        } else if ((passW == "") && (userN == "") && (emailA == "")) {
-            alert("fill in fields ")
-            e.preventDefault();
+    e.preventDefault();
+    let userN = document.getElementById("username").value;
+    let emailA = document.getElementById("email").value;
+    let passW = document.getElementById("password").value;
+
+    if ((passW !== "") && (emailA !== "") && (userN !== "")) {
+        if (passW === key) {
+            document.getElementById("heading").innerHTML = "Correct Sign In";
+        } else {
+            alert("Incorrect password");
+        }
     } else {
-        alert("Incorrect password");
-        e.preventDefault();
+        alert("Fill in all the fields");
     }
 
 }
