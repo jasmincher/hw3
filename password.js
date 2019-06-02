e8fe0a197960106a2ee6e50b85e174ecd8274b59
Jas <c.jasminher@gmail.com>
2019-06-01 21:21:31 -0400

    add: Added function to change heading if password correct, and alert when password incorrect

--- password.js
+++ password.js
@@ -3,12 +3,31 @@
 // b.	if the password is incorrect, send an alert message
 // c.	Your page should also include an <h1> tag. If the information in the form is correct, have JavaScript change the text in the <h1>.HTML page. It should include inputs for a username, email, and password. Also a submit button.
 
-var password = 1234578;
 
-function password() {
-    if (document.getElementById("password").value === password) {
-        document.getElementById("sign-in").innerHTML = "Correct Sign In";
-    }else{
+let key = '12345678';
+let userN = document.getElementById("username").value;
+let emailA = document.getElementById("email").value;
+let passW = document.getElementById("password").value;
+
+
+
+function password(e) {
+    if ((passW == key) && (userN !== "") && (emailA !== "")) {
+        document.getElementById("heading").innerHTML = "Correct Sign In";
+        e.preventDefault();
+        } else if ((passW !== "") && (userN == "") && (emailA == "") && (passW !== key)) {
+            alert("fill in fields and wrong password")
+            e.preventDefault();
+        } else if ((passW == "") && (userN == "") && (emailA == "")) {
+            alert("fill in fields ")
+            e.preventDefault();
+    } else {
         alert("Incorrect password");
+        e.preventDefault();
     }
+
 }
+
+document.getElementById("submit").addEventListener("click", password);
+
+
